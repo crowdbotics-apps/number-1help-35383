b5777935618ab7e1f22f7380cab0fa6a7a872ff4
Crowdbotics <team@crowdbotics.com>
2022-05-22 21:35:20 +0000

    Initial commit.

---
+++ backend/number_1help_35383/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for number_1help_35383 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'number_1help_35383.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "number_1help_35383",
+  url: "https://number_1help_35383.botics.co",
+  api: "https://number_1help_35383.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
